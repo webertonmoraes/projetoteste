c11925a34261673935fea908bd0b3c04d5dc82fe
webertonmoraes <webertonmoraes@gmail.com>
2017-05-30 13:19:44 -0300

    Primeiro commit teste, adicionado o index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        echo 'Olá mundo!';
+        ?>
+    </body>
+</html>
